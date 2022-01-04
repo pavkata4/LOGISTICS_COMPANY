295c5809b6297d1ae4ce9861da0a781444b10bf6
Nikola Popov <nikola.popov01@sap.com>
2022-01-04 19:55:13 +0200

    Introduce new Courier role

--- src/main/java/bg/nbu/logistics/commons/constants/RoleConstants.java
+++ src/main/java/bg/nbu/logistics/commons/constants/RoleConstants.java
@@ -8,9 +8,11 @@ import lombok.NoArgsConstructor;
 public final class RoleConstants {
     public static final String ROLE_ROOT = "ROLE_ROOT";
     public static final String ROLE_EMPLOYEE = "ROLE_EMPLOYEE";
+    public static final String ROLE_COURIER = "ROLE_COURIER";
     public static final String ROLE_USER = "ROLE_USER";
 
     public static final String ROOT = "ROOT";
     public static final String EMPLOYEE = "EMPLOYEE";
+    public static final String COURIER = "COURIER";
     public static final String USER = "USER";
 }
